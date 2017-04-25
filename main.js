764cb69851a7a5c652e3be4151680a45171f111d
farasevych <farasevych@gmail.com>
2017-04-25 15:18:19 +0300

    add

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
 console.log('hello world')
+let compiledHtml = nunjucks.render('main.html', { foo: 'bar' });
+document.getElementById('nunjucks-container').insertAdjacentHTML('afterend', compiledHtml);
